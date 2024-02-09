9b26a5a7f9f4ec34fdecb94d2e14e575e5d63420
ArpanSankesh <arpansankesh830@gmail.com>
2024-02-09 15:00:45 +0530

    responsiveness

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ let flag = 0;
 function toggleHamMenu() {
   if (flag === 0) {
     hamMenu.style.top = 0;
+    hamMenu.style.opacity = 1;
     navBar.style.position = "fixed";
     flag = 1;
 } else {
     hamMenu.style.top = "-100vw";
+    hamMenu.style.opacity = 0;
     navBar.style.position = "relative";
     flag = 0;
   }
