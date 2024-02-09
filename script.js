2261b7e1b923bcc3a139aa498291cc618a0cbe6c
ArpanSankesh <arpansankesh830@gmail.com>
2024-02-09 14:10:59 +0530

    Ham menu done

---
+++ script.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector(".menu-btn");
+const hamMenu = document.querySelector(".ham-menu");
+const navBar = document.querySelector(".nav-bar");
+
+let flag = 0;
+
+function toggleHamMenu() {
+  if (flag === 0) {
+    hamMenu.style.top = 0;
+    navBar.style.position = "fixed";
+    flag = 1;
+} else {
+    hamMenu.style.top = "-100vw";
+    navBar.style.position = "relative";
+    flag = 0;
+  }
+}
+menu.addEventListener("click", toggleHamMenu);
